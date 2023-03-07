1c4cb501e2f79dd922693cc237929c8e45795d2e
Jagdesh <urjagdesh98@gmail.com>
2023-03-07 19:14:04 +0530

    spring crud boilerplate added

---
+++ Java_02_Spring_CRUD/01_CRUD_with_Testing/src/main/java/io/jagdesh/CRUD_with_Testing/CrudWithTestingApplication.java
@@ -0,0 +1,13 @@
+package io.jagdesh.CRUD_with_Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithTestingApplication.class, args);
+	}
+
+}
